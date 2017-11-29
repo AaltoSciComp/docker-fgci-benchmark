d3116708444f642cc975f4218d9ea3dcf8c8198a
Simo Tuomisto <simo.tuomisto@aalto.fi>
2017-11-29 15:54:16 +0200

    Fixed a bug in benchmark.py.

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -7,10 +7,15 @@ import psutil
 with open('/results/parameters.yml','r') as f:
     parameters = load(f.read())
 
-
+ncpus=psutil.cpu_count()
 cpu_info    = cpuinfo.get_cpu_info()
 cpu_flags   = cpu_info['flags']
-ncpus=psutil.cpu_count()
+if 'avx' in cpu_flags:
+    gromacs_version = 'avx256'
+elif 'sse4.1' in cpu_flags:
+    gromacs_version = 'sse4.1'
+else:
+    gromacs_version = 'sse2'
 
 def run_R_GFA(xsize,ysize,learnstart,nlearn,njobs):
     runstring = ' '.join(list(map(str,range(1,njobs+1))))
@@ -20,12 +25,6 @@ def run_R_GFA(xsize,ysize,learnstart,nlearn,njobs):
 
 def run_GROMACS_MPI_testA():
     nsteps = parameters['GROMACS_MPI_testA']['nsteps']
-    if 'avx' in cpu_flags:
-        gromacs_version = 'avx256'
-    elif 'sse4.1' in cpu_flags:
-        gromacs_version = 'sse4.1'
-    else:
-        gromacs_version = 'sse2'
     cmd = 'mpirun -np {0} /benchmarks/gromacs-{1}/bin/gmx_mpi mdrun -s /benchmarks/gromacs-datas/ion_channel.tpr -maxh 0.50 -resethway -noconfout -nsteps {2} -g $(tempfile) -e $(tempfile)'.format(int(ncpus),gromacs_version,nsteps)
     return subprocess.call([cmd],shell=True)
 
