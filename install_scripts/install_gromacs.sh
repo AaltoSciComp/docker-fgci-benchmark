afa4b93ea33bd710084f21e3a6a04bd9b39e2575
Simo Tuomisto <simo.tuomisto@aalto.fi>
2017-12-29 13:14:27 +0200

    Removed gromacs AVX2 installation, as that does not provide benefits.

--- install_scripts/install_gromacs.sh
+++ install_scripts/install_gromacs.sh
@@ -93,32 +93,3 @@ make install
 # Clean up build directory
 cd /tmp && \
 rm -rf gromacs-5.1.4
-
-# Install Gromacs with 256-bit AVX2
-tar xzf gromacs-5.1.4.tar.gz && \
-cd gromacs-5.1.4 && \
-mkdir build && \
-cd build && \
-cmake \
-        -DCMAKE_INSTALL_PREFIX=/benchmarks/gromacs-AVX2_256/ \
-        -DBUILD_SHARED_LIBS=off \
-        -DBUILD_TESTING=off \
-        -DREGRESSIONTEST_DOWNLOAD=OFF \
-        -DCMAKE_C_COMPILER=`which mpicc` \
-        -DCMAKE_CXX_COMPILER=`which mpicxx` \
-        -DGMX_BUILD_OWN_FFTW=on \
-        -DGMX_SIMD=AVX2_256 \
-        -DGMX_DOUBLE=off \
-        -DGMX_EXTERNAL_BLAS=off \
-        -DGMX_EXTERNAL_LAPACK=off \
-        -DGMX_FFT_LIBRARY=fftw3 \
-        -DGMX_GPU=off \
-        -DGMX_MPI=on \
-        -DGMX_OPENMP=on \
-        -DGMX_X11=off \
-        .. && \
-make -j 4 && \
-make install
-
-cd /tmp && \
-rm -rf gromacs-5.1.4 gromacs-5.1.4.tar.gz
