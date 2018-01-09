7171bc021a2f027c5304da9721bcd72e316555f1
Simo Tuomisto <simo.tuomisto@aalto.fi>
2018-01-09 10:14:27 +0200

    Added CP2K benchmark.

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -25,6 +25,9 @@ def run_R_GFA(xsize,ysize,learnstart,nlearn,njobs):
     cmd = 'parallel -j {0} R --vanilla --slave -f /benchmarks/R_GFA.R --args -x {1} -y {2} -l {3} -n {4} -s -- {5}'.format(ncpus,xsize,ysize,learnstart,nlearn,runstring)
     return subprocess.call([cmd],shell=True)
 
+def run_CP2K_MPI_testA():
+    cmd = 'OMP_NUM_THREADS={0} mpirun -np {1} cp2k -i /benchmarks/cp2k-datas/H2O-1024.inp -o /results/cp2k-mpi-testA.out'.format(1,int(ncpus))
+    return subprocess.call([cmd],shell=True)
 
 def run_GROMACS_MPI_testA():
     nsteps = parameters['GROMACS_MPI_testA']['nsteps']
@@ -43,6 +46,10 @@ def test_R_GFA(benchmark):
     njobs = parameters['R_GFA']['njobs']
     nlearn = parameters['R_GFA']['nlearn'] 
     result = benchmark.pedantic(run_R_GFA,args=(xsize,ysize,learnstart,nlearn,njobs),rounds=parameters['R_GFA']['rounds'])
+    assert result == 0
+
+def test_CP2K_MPI_testA(benchmark):
+    result = benchmark.pedantic(run_CP2K_MPI_testA,rounds=parameters['CP2K_MPI_testA'].get('rounds',1))
     assert result == 0
 
 def test_GROMACS_MPI_testA(benchmark):
