d82cbc629a5747805af3e26f39edd861675518c3
Janne Blomqvist <janne.blomqvist@aalto.fi>
2019-02-07 18:21:49 +0200

    Update cp2k version

--- install_scripts/install_cp2k.sh
+++ install_scripts/install_cp2k.sh
@@ -8,6 +8,6 @@ git clone https://github.com/spack/spack.git spack
 
 . spack/share/spack/setup-env.sh
 
-export SPACK_TARGET_TYPE="x86_64_avx"
+#export SPACK_TARGET_TYPE="x86_64_avx"
 
-spack install cp2k@4.1 smm=none
+spack install cp2k@6.1
