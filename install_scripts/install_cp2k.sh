455c636984da060949478571ef4fa481bf95a3eb
Janne Blomqvist <janne.blomqvist@aalto.fi>
2019-02-26 16:53:28 +0200

    Use OpenBLAS 0.3.5 for cp2k with disabled avx-512

--- install_scripts/install_cp2k.sh
+++ install_scripts/install_cp2k.sh
@@ -5,7 +5,7 @@ cd /benchmarks
 # Install Spack
 
 git clone https://github.com/spack/spack.git spack
-patch -p0 < spack.patch
+patch -p1 -dspack < spack.patch
 
 # Alternatively, a released version:
 #curl -OL https://github.com/spack/spack/releases/download/v0.12.1/spack-0.12.1.tar.gz
@@ -19,4 +19,6 @@ patch -p0 < spack.patch
 
 # Install openblas devel version, 0.3.5 AVX-512 kernels are b0rken
 # Unfortunately, so are 0.3.6dev AVX-512 kernels as of 2019-02-25.
-spack install cp2k@6.1 ^openblas@develop
+#spack install cp2k@6.1 ^openblas@develop
+# In spack.patch we disable AVX-512 kernels and build 0.3.5
+spack install cp2k@6.1
